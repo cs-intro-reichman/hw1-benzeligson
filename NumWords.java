3f9fca0388ae6ab8264422f25caab100b76c899f
Benjamin Zeligson <benjamin.zeligson@post.runi.ac.il>
2025-11-03 14:19:32 +0200

    Completed assignment

--- NumWords.java
+++ NumWords.java
@@ -3,10 +3,8 @@ public class NumWords {
 	public static void main(String args[]) {
 	    int num = Integer.parseInt(args[0]);
 		int ones = num % 10;
-		int tens = num % 100;
-		tens = tens / 10;
-		int hundreds = num % 1000;
-		hundreds = hundreds / 100;
+		int tens = (num % 100) / 10;
+		int hundreds = (num % 1000) / 100;
 		System.out.println(hundreds + " hundreds, " + tens + 
 						   " tens, and " + ones + " ones.");
 	}
